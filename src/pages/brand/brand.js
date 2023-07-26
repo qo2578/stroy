b2d2a5ee0825e4349626781de0c1bef863b06f62
iskenderovaAiperi <132426791+iskenderovaAiperi@users.noreply.github.com>
2023-07-26 23:45:08 +0600

    обновила структуру

--- src/pages/brand/brand.js
+++ src/pages/brand/brand.js
@@ -1,9 +1,27 @@
+// $(document).ready(function() {
+//     $('ul.brand-tabs__caption').on('click', 'li:not(.brand-tabs__btn_active)', function() {
+//         $(this)
+//           .addClass('.brand-tabs__btn_active').siblings().removeClass('.brand-tabs__btn_active')
+//           .closest('div.brand-tabs').find('div.brand-tabs__content').removeClass('brand-tabs__content_active').eq($(this).index()).addClass('brand-tabs__content_active');
+//       });
+
+// })
+
+// const brandTabs = document.querySelector('.brand-tabs');
+// const firstLi = brandTabs.querySelector('.brand-tabs__btn:first-child');
+
+// firstLi.addEventListener('click', function() {
+//   const allContent = document.querySelectorAll('.brand-tabs__content');
+//   allContent.forEach(function(content) {
+//     content.style.display = '';
+//   });
+// });
 $(document).ready(function() {
-    $('ul.brand-tabs__caption').on('click', 'li:not(.brand-tabs__btn_active)', function() {
-        $(this)
-          .addClass('.brand-tabs__btn_active').siblings().removeClass('.brand-tabs__btn_active')
-          .closest('div.brand-tabs').find('div.brand-tabs__content').removeClass('brand-tabs__content_active').eq($(this).index()).addClass('brand-tabs__content_active');
-      });
+  $('ul.brand-tabs__caption').on('click', 'li:not(.brand-tabs__btn_active)', function() {
+      $(this)
+        .addClass('.brand-tabs__btn_active').siblings().removeClass('.brand-tabs__btn_active')
+        .closest('div.brand-tabs').find('div.brand-tabs__content').removeClass('brand-tabs__content_active').eq($(this).index()-1).addClass('brand-tabs__content_active');
+    });
 
 })
 
@@ -11,8 +29,8 @@ const brandTabs = document.querySelector('.brand-tabs');
 const firstLi = brandTabs.querySelector('.brand-tabs__btn:first-child');
 
 firstLi.addEventListener('click', function() {
-  const allContent = document.querySelectorAll('.brand-tabs__content');
-  allContent.forEach(function(content) {
-    content.style.display = 'block';
-  });
+const allContent = document.querySelectorAll('.brand-tabs__content');
+allContent.forEach(function(content) {
+  content.classList.add('brand-tabs__content_active')
+});
 });

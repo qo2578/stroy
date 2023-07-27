e1572a725f5a03aed8ff2e5415185940301d2af6
iskenderovaAiperi <132426791+iskenderovaAiperi@users.noreply.github.com>
2023-07-27 11:17:41 +0600

    обновила структуру

--- src/js/main.js
+++ src/js/main.js
@@ -63,18 +63,47 @@ $(document).ready(function () {
     $(window).on('scroll', function () {
         if ($(window).scrollTop() > 500) {
             $('.to-top-btn').fadeIn()
-        } else{
+        } else {
             $('.to-top-btn').fadeOut()
         }
     })
 });
+
+
+// const addToCartBtn = document.querySelectorAll('.bestsellers__box-general-btn');
+// const counter = document.querySelectorAll('.counter');
+// const minusBtn = document.querySelectorAll('.counter__minus');
+// const plusBtn = document.querySelectorAll('.counter__plus');
+// const counterValue = document.querySelectorAll('.counter__value');
+
+// let count = 0;
+
+// addToCartBtn.forEach((btn, index) => {
+//     btn.addEventListener('click', function (event) {
+//         event.preventDefault();
+//         addToCartBtn[index].style.display = 'none';
+//         counter[index].style.display = 'flex';
+//     });
+
+//     minusBtn[index].addEventListener('click', function () {
+//         if (count > 0) {
+//             count--;
+//             counterValue[index].textContent = count;
+//         }
+//     });
+
+//     plusBtn[index].addEventListener('click', function () {
+//         count++;
+//         counterValue[index].textContent = count;
+//     });
+// })
+
+
 const addToCartBtn = document.querySelector('.bestsellers__box-general-btn');
 const counter = document.querySelector('.counter');
 const minusBtn = document.querySelector('.counter__minus');
 const plusBtn = document.querySelector('.counter__plus');
-const counterValue = document.querySelector('.counter__value');
-
-let count = 0;
+const counterInput = document.querySelector('.counter__input');
 
 addToCartBtn.addEventListener('click', function(event) {
   event.preventDefault();
@@ -83,13 +112,27 @@ addToCartBtn.addEventListener('click', function(event) {
 });
 
 minusBtn.addEventListener('click', function() {
+  let count = parseInt(counterInput.value);
   if (count > 0) {
     count--;
-    counterValue.textContent = count;
+    counterInput.value = count;
   }
 });
 
 plusBtn.addEventListener('click', function() {
+  let count = parseInt(counterInput.value);
   count++;
-  counterValue.textContent = count;
+  counterInput.value = count;
 });
+
+counterInput.addEventListener('input', function() {
+  let count = parseInt(counterInput.value);
+  if (isNaN(count) || count < 0) {
+    counterInput.value = 0;
+  }
+});
+
+
+
+document.querySelector("counter__input").addEventListener("keyup", function () {
+    this.value = this.value.replace(/[^\d]/g, "");})
